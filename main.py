fd678e18d9ba0c17698f328154d747936e12c13c
ViezTrinker <muham@DESKTOP-PKQMJ32>
2023-09-17 15:51:25 +0200

    Initiliazed objects in the right way

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import matplotlib.pyplot as plt
 import time
+import random
 
 # Declarations
-numprey      = 2000
-numpredator  = 2000
-numplant     = 2000
-lenghtx      = 100
-lengthy      = 100
+numprey         = 2000
+numpredator     = 2000
+numplant        = 2000
+lengthx         = 100
+lengthy         = 100
+maxspeed        = 10
+maxvision       = 10
+maxconsumption  = 10
+maxattack       = 10
+
+maxstatprey = 40
+maxstatpred = 40
 
 aliveprobprey  = 0.1
 aliveprobpred  = 0.1
@@ -23,6 +31,19 @@ class Prey():
         self.vision = vision
         self.consumption = consumption
         self.attack = attack
+        self.normalize()
+    def setvalues(self, speed, vision, consumption, attack):
+        self.speed = speed
+        self.vision = vision
+        self.consumption = consumption
+        self.attack = attack
+    def normalize(self):
+        speed = self.speed / (self.speed + self.vision + self.consumption + self.attack) * maxstatprey
+        vision = self.vision / (self.speed + self.vision + self.consumption + self.attack) * maxstatprey
+        consumption = self.consumption / (self.speed + self.vision + self.consumption + self.attack) * maxstatprey
+        attack = self.attack / (self.speed + self.vision + self.consumption + self.attack) * maxstatprey
+        self.setvalues(speed, vision, consumption, attack)
+
 
 class Predator():
     def __init__(self, alive, x, y, speed, vision, consumption, attack):
@@ -33,6 +54,20 @@ class Predator():
         self.vision = vision
         self.consumption = consumption
         self.attack = attack
+        self.normalize()
+
+    def setvalues(self, speed, vision, consumption, attack):
+        self.speed = speed
+        self.vision = vision
+        self.consumption = consumption
+        self.attack = attack
+
+    def normalize(self):
+        speed = self.speed / (self.speed + self.vision + self.consumption + self.attack) * maxstatpred
+        vision = self.vision / (self.speed + self.vision + self.consumption + self.attack) * maxstatpred
+        consumption = self.consumption / (self.speed + self.vision + self.consumption + self.attack) * maxstatpred
+        attack = self.attack / (self.speed + self.vision + self.consumption + self.attack) * maxstatpred
+        self.setvalues(speed, vision, consumption, attack)
 
 class Plant():
     def __init__(self, alive, x, y):
@@ -40,33 +75,36 @@ class Plant():
         self.x = x
         self.y = y
 
+def InitializeObjects():
+    for x in range(numprey):
+        prey[x] = Prey(random.random() <= aliveprobprey, random.random() * lengthx, random.random() * lengthy,
+                       random.random() * maxspeed, random.random() * maxvision, random.random() * maxconsumption,
+                       random.random() * maxattack)
+
+    for x in range(numpredator):
+        predator[x] = Predator(random.random() <= aliveprobpred, random.random() * lengthx, random.random() * lengthy,
+                               random.random() * maxspeed, random.random() * maxvision,
+                               random.random() * maxconsumption, random.random() * maxattack)
+
+    for x in range(numplant):
+        plant[x] = Plant(random.random() <= aliveprobplant, random.random() * lengthx, random.random() * lengthy)
+
 # Create objects
 
 prey     = [0] * numprey
 predator = [0] * numpredator
 plant    = [0] * numplant
 
-for x in range(numprey):
-    # TODO: initialize in the right way
-    prey[x] = Prey(True, 1, 2, 3, 4, 5, 6)
-
-for x in range(numpredator):
-    # TODO: initialize in the right way
-    predator[x] = Predator(True, 1, 2, 3, 4, 5, 6)
-
-for x in range(numplant):
-    # TODO: initialize in the right way
-    plant[x] = Plant(True, 1, 2)
-
+# START #
+InitializeObjects()
 # Main Loop
-
 while True:
     # TODO: Implement the whole logic
     print("I am running")
 
     # TODO: Plot graph in the right way
-    plt.plot([1, 2])
-    plt.show()
+    # plt.plot([1, 2])
+    # plt.show()
 
     time.sleep(1)
 
